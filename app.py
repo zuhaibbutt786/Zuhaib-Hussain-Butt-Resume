1a9036dfb84af61911bcf669874d118a5a98eb59
zuhaibbutt786 <67970420+zuhaibbutt786@users.noreply.github.com>
2023-03-30 10:32:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ profile_pic = current_dir / "assets" / "zuhaib.png"
 
 # --- GENERAL SETTINGS ---
 PAGE_TITLE = "Digital CV | Zuhaib Hussain Butt"
-PAGE_ICON = ":sentiment_satisfied:"
+PAGE_ICON = ":wave:"
 NAME = "Zuhaib Hussain Butt"
 DESCRIPTION = """
 Data Analyst,Machine Learning Engineer assisting enterprises by supporting data-driven decision-making.
